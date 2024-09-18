a49c2b61e271474f4e1d3f06e33fa62731bb3ce2
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-09-18 16:39:16 +0200

    [UPD] Created class movie

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    class movie{
+        public $title;
+        public $genre;
+        public $year;
+
+        function __construct($_title,$_genre,$_year){
+            $this->title = $_title;
+            $this->genre = $_genre;
+            $this->year = $_year;
+        }
+
+        public function getMovieInfo(){
+            return $this->title." ".$this->genre." ".$this->year;
+        }
+    }
+?>
